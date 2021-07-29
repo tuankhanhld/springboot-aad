e5dbc5977c9fae5dbd848ab4c7853f84106c8c3b
ntuankhanh <ntuankhanh@tma.com.vn>
2021-07-29 14:18:48 +0700

    initial commit

---
+++ src/main/java/com/example/activedirectorywebappresource/ActiveDirectoryWebappResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.activedirectorywebappresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveDirectoryWebappResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveDirectoryWebappResourceApplication.class, args);
+    }
+
+}
